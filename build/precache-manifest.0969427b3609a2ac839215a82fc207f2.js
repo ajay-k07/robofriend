ad3900674b8f6e308405fd9f67f909e8990121ea
ajai lee <ajaidonglee@gmail.com>
2020-07-29 10:45:22 +0530

    deploy

---
+++ build/precache-manifest.0969427b3609a2ac839215a82fc207f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da9f14ea89a990cfe044096e14172ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68029cae79872d730ebf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "439fe6fb506aa76c043e",
+    "url": "/static/css/main.5f618c51.chunk.css"
+  },
+  {
+    "revision": "68029cae79872d730ebf",
+    "url": "/static/js/2.d2e9c624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2e9c624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439fe6fb506aa76c043e",
+    "url": "/static/js/main.8402bacc.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
